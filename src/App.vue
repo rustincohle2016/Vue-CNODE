34b1abef751cdf150e1420aaee02836d853cddfb
rustincohle <rustincohle@foxmail.com>
2019-01-08 22:01:58 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+  <Header></Header>
+  <Postlist></Postlist>
+    <!--注意写完组件后要在app组件进行渲染-->
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header'
+  import Postlist from './components/Postlist'
+export default {
+  name: 'App',
+  components:{
+    Header,Postlist
+    // components组件内也要进行引用
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
