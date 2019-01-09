525d930052aa62b950912ad46276150c4e174f59
rustincohle <rustincohle@foxmail.com>
2019-01-09 22:45:06 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Article from "../components/Article";
+import Postlist from "../components/Postlist";
+import Userinfo from "../components/Userinfo"
 
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: '',
-    //   component:
-    // }
+    {
+      name: 'root',
+      path: '/',
+      components: {
+        // 注意这里使用components!!!!
+        main: Postlist
+      }
+    },
+
+    {
+      path: '/topic/:id',
+      name: 'post_content',
+      components:{
+        main:Article,
+        // 组件定义名称
+      }
+    },
+    // 此对象是postlist的路由
+    {
+      name:'user_info',
+      path:'/userinfo/name',
+      components:{
+        main:Userinfo,
+      }
+    }
   ]
 })
